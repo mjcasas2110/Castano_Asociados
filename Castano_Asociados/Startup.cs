0c47d4b27446d40bba3243a08b4864cf42ec4946
Manuel C <casasmanuj@gmail.com>
2016-01-10 15:22:36 -0300

    Primer commit. Se sube el proyecto a GitHub

---
+++ Castano_Asociados/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Castano_Asociados.Startup))]
+namespace Castano_Asociados
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
