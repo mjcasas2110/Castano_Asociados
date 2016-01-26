c76ebc8b7634cad7bcf01727474b4271850b1f73
Manuel C <casasmanuj@gmail.com>
2016-01-26 00:39:24 -0300

    Se mejora la vista Home. TODO: Investigar como poner la background image

---
+++ Castano_Asociados/Models/Cliente.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+
+namespace Castano_Asociados.Models
+{
+    [Table("CLIENTE")]
+    public class Cliente
+    {
+        [Key]
+        [Column("ID")]
+        public int Id { get; set; }
+        [Column("NOMBRE")]
+        public string Nombre { get; set; }
+        [Column("EMAIL")]
+        public string Email { get; set; }
+        [Column("PAGINA_WEB")]
+        [Display(Name="Pagina Web")]
+        public string Pagina_Web { get; set; }
+
+        public virtual ICollection<Evento> Eventos { get; set; }
+
+        public Cliente()
+        {
+            Eventos = new HashSet<Evento>();
+        }
+    }
+}
---
+++ Castano_Asociados/Models/Evento.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+
+namespace Castano_Asociados.Models
+{
+    [Table("EVENTO")]
+    public class Evento
+    {
+        [Key]
+        [Column("ID")]
+        public int Id { get; set; }
+
+        [Column("ID_CLIENTE")]
+        [Display(Name = "Cliente")]
+        public int IdCliente { get; set; }
+
+        [Column("FECHA_INICIO")]
+        public DateTime Fecha_Inicio { get; set; }
+
+        [Column("FECHA_FIN")]
+        public DateTime Fecha_Fin { get; set; }
+
+        public virtual Cliente Cliente { get; set; }
+    }
+}
